f910c66c9ff880d4c9be5f9dea9fbd9334f2c0fd
abc9207144 <sanecrtk@gmail.com>
2023-04-15 19:15:54 +0800

    fix the ios calculator and remove css depandancy

--- js/app.js
+++ js/app.js
@@ -1,166 +1,24 @@
-// DOM Elements
+function number(value){
+	document.mf.output.value += value;
+	}
+function remove(){
+    document.mf.output.value=" ";
+     }   
 
-const valueEl = document.querySelector('.display');
-const acEl = document.querySelector('.clear');
-const pmEl = document.querySelector('.plusmn');
-const percentEl = document.querySelector('.percentange');
+ function equal(){
+    document.mf.output.value=eval(document.mf.output.value);
+     }
 
-const addEl = document.querySelector('.add');
-const subtractEl = document.querySelector('.subtract');
-const multipleEl = document.querySelector('.multiple');
-const divideEl = document.querySelector('.divide');
-const equalEl = document.querySelector('.equal');
-
-const decimalEl = document.querySelector('.decimal');
-const number0El = document.querySelector('.number0');
-const number1El = document.querySelector('.number1');
-const number2El = document.querySelector('.number2');
-const number3El = document.querySelector('.number3');
-const number4El = document.querySelector('.number4');
-const number5El = document.querySelector('.number5');
-const number6El = document.querySelector('.number6');
-const number7El = document.querySelector('.number7');
-const number8El = document.querySelector('.number8');
-const number9El = document.querySelector('.number9');
-const numberElArray = [
-  number0El, number1El, number2El, number3El, number4El,
-  number5El, number6El, number7El, number8El, number9El
-];
-
-
-// variables
-let valueStrInMemory = null;
-let operatorInMemory = null;
-
-
-// Functions
-const getValueAsStr = () => valueEl.textContent.split(',').join('');
-
-const getValueAsNum = () => {
-  return parseFloat(getValueAsStr());
-};
-
-const setStrAsValue = (valueStr) => {
-  if (valueStr[valueStr.length - 1] === '.') {
-    valueEl.textContent += '.';
-    return;
-  }
-
-  const [wholeNumStr, decimalStr] = valueStr.split('.');
-  if (decimalStr) {
-    valueEl.textContent =
-      parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
-  } else {
-    valueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
-  }
-};
-
-const handleNumberClick = (numStr) => {
-  const currentValueStr = getValueAsStr();
-  if (currentValueStr === '0') {
-    setStrAsValue(numStr);
-  } else {
-    setStrAsValue(currentValueStr + numStr);
-  }
-};
-
-const getResultOfOperationAsStr = () => {
-  const currentValueNum = getValueAsNum();
-  const valueNumInMemory = parseFloat(valueStrInMemory);
-  let newValueNum;
-  if (operatorInMemory === 'add') {
-    newValueNum = valueNumInMemory + currentValueNum;
-  } else if (operatorInMemory === 'subtract') {
-    newValueNum = valueNumInMemory - currentValueNum;
-  } else if (operatorInMemory === 'multiple') {
-    newValueNum = valueNumInMemory * currentValueNum;
-  } else if (operatorInMemory === 'divide') {
-    newValueNum = valueNumInMemory / currentValueNum;
-  }
-
-  return newValueNum.toString();
-};
-
-const handleOperatorClick = (operation) => {
-  const currentValueStr = getValueAsStr();
-
-  if (!valueStrInMemory) {
-    valueStrInMemory = currentValueStr;
-    operatorInMemory = operation;
-    setStrAsValue('0');
-    return;
-  }
-  valueStrInMemory = getResultOfOperationAsStr();
-  operatorInMemory = operation;
-  setStrAsValue('0');
-};
-
-
-
-
-// Add Event Listeners to functions
-acEl.addEventListener('click', () => {
-  setStrAsValue('0');
-  valueStrInMemory = null;
-  operatorInMemory = null;
-});
-pmEl.addEventListener('click', () => {
-  const currentValueNum = getValueAsNum();
-  const currentValueStr = getValueAsStr();
-
-  if (currentValueStr === '-0') {
-    setStrAsValue('0');
-    return;
-  }
-  if (currentValueNum >= 0) {
-    setStrAsValue('-' + currentValueStr);
-  } else {
-    setStrAsValue(currentValueStr.substring(1));
-  }
-});
-percentEl.addEventListener('click', () => {
-  const currentValueNum = getValueAsNum();
-  const newValueNum = currentValueNum / 100;
-  setStrAsValue(newValueNum.toString());
-  valueStrInMemory = null;
-  operatorInMemory = null;
-});
-
-
-// add event listeners to operators
-addEl.addEventListener('click', () => {
-  handleOperatorClick('add');
-});
-subtractEl.addEventListener('click', () => {
-  handleOperatorClick('subtract');
-});
-multipleEl.addEventListener('click', () => {
-  handleOperatorClick('multiple');
-});
-divideEl.addEventListener('click', () => {
-  handleOperatorClick('divide');
-});
-equalEl.addEventListener('click', () => {
-  if (valueStrInMemory) {
-    setStrAsValue(getResultOfOperationAsStr());
-    valueStrInMemory = null;
-    operatorInMemory = null;
-  }
-});
-
-
-// Add Event Listeners to numbers and decimal
-for (let i=0; i < numberElArray.length; i++) {
-  const numberEl = numberElArray[i];
-  numberEl.addEventListener('click', () => {
-    handleNumberClick(i.toString());
-  });
+function present(){
+    document.mf.output.value = document.mf.output.value/10;
 }
-decimalEl.addEventListener('click', () => {
-  const currentValueStr = getValueAsStr();
-  if (!currentValueStr.includes('.')) {
-    setStrAsValue(currentValueStr + '.');
-  }
-});
 
 
+function pm(){
+    if(document.mf.output.value>0){
+        document.mf.output.value= -document.mf.output.value;
+    }
+    else{
+        document.mf.output.value=Math.abs(document.mf.output.value);
+    }
+}
--- js/sc.js
+++ js/sc.js
@@ -1,44 +1,46 @@
 function sin(){
-	document.calculator.result.value=Math.sin(document.calculator.result.value);
+	document.calculator.output.value=Math.sin(document.calculator.output.value);
 	}
 
 	function cos(){
-	document.calculator.result.value=Math.cos(document.calculator.result.value);
+	document.calculator.output.value=Math.cos(document.calculator.output.value);
 	}
 
 	function tan(){
-	document.calculator.result.value=Math.tan(document.calculator.result.value);
+	document.calculator.output.value=Math.tan(document.calculator.output.value);
 	}
 
 	function BACKSPC(){
 	var a = document.calculator.result.value;
-	document.calculator.result.value = a.substr(0, a.length-1);
+	document.calculator.output.value = a.substr(0, a.length-1);
 	}
 
 	function square(){
-	document.calculator.result.value = Math.pow(document.calculator.result.value, 2);
+	document.calculator.output.value = Math.pow(document.calculator.output.value, 2);
 	}
 
 	function cubed(){
-	document.calculator.result.value = Math.pow(document.calculator.result.value, 3);
+	document.calculator.output.value = Math.pow(document.calculator.output.value, 3);
 	}
 
 	function sqrt2(){
-	document.calculator.result.value = Math.pow(document.calculator.result.value, 1/2);
+	document.calculator.output.value = Math.pow(document.calculator.output.value, 1/2);
 	}
 
 	function sqrt3(){
-	document.calculator.result.value = Math.pow(document.calculator.result.value, 1/3);
+	document.calculator.output.value = Math.pow(document.calculator.output.value, 1/3);
 	}
 
 	function number(value){
-	document.calculator.result.value += value;
+	document.calculator.output.value += value;
 	}
 
 	function remove(){
-	document.calculator.result.value=" ";
+	document.calculator.output.value=" ";
 	}
 
 	function equal(){
-	document.calculator.result.value=eval(document.calculator.result.value);
+	document.calculator.output.value=eval(document.calculator.output.value);
 	}
+
+	
